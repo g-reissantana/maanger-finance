f1e9b5894d3dd01983df321794382ed71b92a014
gabriel_reiss <gabriel.reis@grupoviadigital.com>
2022-09-04 10:37:23 -0300

    started project

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+
+export const Home = () => {
+
+    return(
+        <View>
+            <Text>
+                Homepage
+            </Text>
+        </View>
+    )
+}
